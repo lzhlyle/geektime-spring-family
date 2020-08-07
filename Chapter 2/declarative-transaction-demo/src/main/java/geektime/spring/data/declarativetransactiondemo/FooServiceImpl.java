040ad8b44fd1240f8f109509e7c76c23d623e661
lzhlyle <lzhlyle@msn.cn>
2020-08-07 19:02:15 +0800

    Update: Injection FooService itself.

--- Chapter 2/declarative-transaction-demo/src/main/java/geektime/spring/data/declarativetransactiondemo/FooServiceImpl.java
+++ Chapter 2/declarative-transaction-demo/src/main/java/geektime/spring/data/declarativetransactiondemo/FooServiceImpl.java
@@ -1,14 +1,19 @@
 package geektime.spring.data.declarativetransactiondemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@Slf4j
 public class FooServiceImpl implements FooService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+//    @Autowired
+//    private FooService self;
 
     @Override
     @Transactional
@@ -26,5 +31,7 @@ public class FooServiceImpl implements FooService {
     @Override
     public void invokeInsertThenRollback() throws RollbackException {
         insertThenRollback();
+//        log.info(self.getClass().getName());
+//        self.insertThenRollback();
     }
 }
